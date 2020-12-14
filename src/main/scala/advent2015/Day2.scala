b926e47413ac11fdd5feb1f3d6d9cec4549a4220
Owen Davies <owen@odavi.es>
2020-12-14 20:58:30 +0000

    Add day 2

---
+++ src/main/scala/advent2015/Day2.scala
@@ -0,0 +1,47 @@
+package advent2015
+
+object Day2 {
+    def main(args: Array[String]) = {
+        val dims = parseInput()
+
+        println(part1(dims))
+        println(part2(dims))
+    }
+
+    def part1(dims: List[Array[Int]]): Int = {
+        var total = 0
+
+        for (pres <- dims) {
+            val l = pres(0)
+            val w = pres(1)
+            val h = pres(2)
+
+            total += (2*l*w + 2*w*h + 2*h*l)
+
+            scala.util.Sorting.quickSort(pres)
+
+            total += (pres(0) * pres(1))
+        }
+
+        return total
+    }
+
+    def part2(dims: List[Array[Int]]): Int = {
+        var total = 0
+
+        for (pres <- dims) {
+            scala.util.Sorting.quickSort(pres)
+
+            total += (2*pres(0) + 2*pres(1))
+            total += pres.product
+        }
+
+        return total
+    }
+
+    private def parseInput(): List[Array[Int]] = io.Source
+        .fromResource("day2")
+        .getLines()
+        .toList
+        .map { _.split("x").toArray.map(_.toInt) }
+}
