04d4432a141fba4776a66b704d876ade54bc8dea
Owen Davies <owen@odavi.es>
2020-12-17 19:18:56 +0000

    Add Day 6 part 2

--- src/main/scala/advent2015/Day6.scala
+++ src/main/scala/advent2015/Day6.scala
@@ -9,27 +9,21 @@ object Day6 {
         val lines = Problem.parseInputToList("day6")
 
         println(part1(lines))
+        println(part2(lines))
     }
 
     def part1(lines: List[String]): Int = {
-        val parser = 
-            """(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)""".r
-        var data = new ListBuffer[
-            Tuple3[String, Tuple2[Int, Int], Tuple2[Int, Int]]
-        ]()
-
-        lines.foreach(l => {
-            val parser(comm, sx, ex, sy, ey) = l
-            data += Tuple3(
-                comm.toString, 
-                Tuple2(sx.toInt, ex.toInt),
-                Tuple2(sy.toInt, ey.toInt)
-            )
-        })
-
+        val data = populateData(lines)
         var state = Array.fill[Int](1000000)(0)
         data.foreach(i => state = change(i._1, state, i._2, i._3))
         return countLights(state)
+    }
+
+    def part2(lines: List[String]): Int = {
+        val data = populateData(lines)
+        var state = Array.fill[Int](1000000)(0)
+        data.foreach(i => state = changeBrightness(i._1, state, i._2, i._3))
+        return state.sum
     }
 
     def change
@@ -38,10 +32,10 @@ object Day6 {
         , start: Tuple2[Int, Int]
         , end: Tuple2[Int, Int])
         : Array[Int] = {
-        
+
         val indices = getLights(start, end)
 
-        command match { 
+        command match {
             case "turn on"  => indices.foreach( i => state(i) = 1 )
             case "turn off" => indices.foreach( i => state(i) = 0 )
             case "toggle"   => indices.foreach( i => state(i) = 1 - state(i) )
@@ -50,7 +44,49 @@ object Day6 {
         return state
     }
 
+    def changeBrightness
+        (command: String
+        , state: Array[Int]
+        , start: Tuple2[Int, Int]
+        , end: Tuple2[Int, Int])
+        : Array[Int] = {
+
+        val indices = getLights(start, end)
+
+        command match {
+            case "turn on"  =>
+                indices.foreach( i => state(i) += 1 )
+            case "turn off" =>
+                indices.foreach( i => state(i) = 0.max(state(i) - 1) )
+            case "toggle"   =>
+                indices.foreach( i => state(i) += 2 )
+        }
+
+        return state
+    }
+
     def countLights(state: Array[Int]) = state.filter(_ > 0).size
+
+    private def populateData
+                (lines: List[String])
+                : List[Tuple3[String, Tuple2[Int, Int], Tuple2[Int, Int]]] = {
+        val parser =
+            """(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)""".r
+        var data = new ListBuffer[
+            Tuple3[String, Tuple2[Int, Int], Tuple2[Int, Int]]
+        ]()
+
+        lines.foreach(l => {
+            val parser(comm, sx, ex, sy, ey) = l
+            data += Tuple3(
+                comm.toString,
+                Tuple2(sx.toInt, ex.toInt),
+                Tuple2(sy.toInt, ey.toInt)
+            )
+        })
+
+        return data.toList
+    }
 
     private def getLights
         (start: Tuple2[Int, Int]
@@ -62,7 +98,7 @@ object Day6 {
         val endy   = end._2
 
         var indices = ListBuffer[Int]()
-   
+
         for (x <- startx to endx) {
             for (y <- starty to endy) {
                 indices += (x * length + y)
--- src/test/scala/Day6.scala
+++ src/test/scala/Day6.scala
@@ -5,13 +5,12 @@ import org.scalatest.funsuite.AnyFunSuite
 class Day6Spec extends AnyFunSuite {
 
     test("Day 6 Part 1") {
-        
         assertResult(1000000) {
             Day6.countLights(Day6.change(
                 "turn on", getAllOffInput(), (0, 0), (999, 999)
             ))
         }
-        
+
         assertResult(1000) {
             Day6.countLights(Day6.change(
                 "toggle", getAllOffInput(), (0, 0), (999, 0)
@@ -22,6 +21,20 @@ class Day6Spec extends AnyFunSuite {
             Day6.countLights(Day6.change(
                 "turn off", getAllOnInput(), (499, 499), (500, 500)
             ))
+        }
+    }
+
+    test("Day 6 Part 2") {
+        assertResult(1) {
+            Day6.changeBrightness(
+                "turn on", getAllOffInput(), (0, 0), (0, 0)
+            ).sum
+        }
+
+        assertResult(2000000) {
+            Day6.changeBrightness(
+                "toggle", getAllOffInput(), (0, 0), (999, 999)
+            ).sum
         }
     }
 
