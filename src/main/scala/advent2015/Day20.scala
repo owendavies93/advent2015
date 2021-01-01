dd5bc16cd54ea7c573c5bf6e2dc64641244025db
Owen Davies <owen@odavi.es>
2021-01-01 19:33:15 +0000

    Add day 20

---
+++ src/main/scala/advent2015/Day20.scala
@@ -0,0 +1,36 @@
+package advent2015
+
+import scala.collection.mutable.Map
+
+object Day20 {
+
+    def main(args: Array[String]) = {
+        println(part1(33100000))
+        println(part2(33100000))
+    }
+
+    def part1(target: Int):Int = {
+        // Only bother checking multiples of 6, since they have far more
+        // factors than multiples of other low numbers
+        return Stream.from(6, 6).dropWhile(i => {
+            Factorise.factors(i).sum * 10 < target
+        })(0)
+    }
+
+    def part2(target: Int):Int = {
+        var deliveredCounts = Map[Int, Int]()
+        // The factors of 6 trick doesn't work here since we need to
+        // count the occurrences of each factor
+        return Stream.from(1).dropWhile(i => {
+            val factors = Factorise.factors(i)
+            val filtered = factors.filter(f => {
+                deliveredCounts.getOrElse(f, 0) <= 50
+            })
+            factors.foreach(f => {
+                val count = deliveredCounts.getOrElse(f, 0)
+                deliveredCounts(f) = count + 1
+            })
+            filtered.sum * 11 < target
+        })(0)
+    }
+}
---
+++ src/test/scala/Day20.scala
@@ -0,0 +1,12 @@
+package advent2015
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class Day20Spec extends AnyFunSuite {
+
+    test("Day20 part 1") {
+        assertResult(6) {
+            Day20.part1(100)
+        }
+    }
+}
