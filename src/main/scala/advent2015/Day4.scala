c72f0771177c2ee172b315949778a3b169e7da9a
Owen Davies <owen@odavi.es>
2021-01-14 15:09:26 +0000

    Use hashing utils in day 4

--- src/main/scala/advent2015/Day4.scala
+++ src/main/scala/advent2015/Day4.scala
@@ -1,11 +1,9 @@
 package advent2015
 
+import scalaadventutils.Hashing
 import scalaadventutils.Problem
 
-import java.security.MessageDigest
-
 object Day4 {
-    val digest = MessageDigest.getInstance("MD5")
 
     def main(args: Array[String]) = {
         val input = Problem.parseInputToString("day4")
@@ -14,25 +12,10 @@ object Day4 {
         println(part2(input))
     }
 
-    def part1(input: String): Int = {
-        return findWithZeros(input, 1, 5)
-    }
+    def part1(input: String): Int =
+        Hashing.findHashWithLeadingChars(input, 1, 5, 0)
 
-    def part2(input: String): Int = {
-        return findWithZeros(input, 1, 6)
-    }
-
-    private def findWithZeros(input: String, candidate: Int, zeros: Int): Int =
-        if (checkZeros(
-             digest.digest((input + candidate.toString).getBytes),
-             zeros)
-           ) return candidate
-        else findWithZeros(input, candidate + 1, zeros)
-
-    def checkZeros(input: Array[Byte], zeros: Int): Boolean = {
-        if (zeros % 2 == 0) (0 until zeros / 2).forall(input(_) == 0)
-        else (0 until (zeros - 1) / 2).forall(input(_) == 0) &&
-             ((input((zeros - 1) / 2) & 0xF0) == 0)
-    }
+    def part2(input: String): Int =
+        Hashing.findHashWithLeadingChars(input, 1, 6, 0)
 
 }
